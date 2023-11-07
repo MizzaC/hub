8d972d04ef296d80907d5ebac0726317901e7cd2
Cazeva <72704263+Cazeva@users.noreply.github.com>
2023-11-07 20:36:03 +0100

    Login finir view et form dans dashboard

--- Mizzac/DashBoard/urls.py
+++ Mizzac/DashBoard/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import SignUpView
 
 urlpatterns = [
+    path('login/', LogInView.as_view(), name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    
+
 ]
