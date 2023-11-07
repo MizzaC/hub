bea9eac92632b9c627a62ccbeada47429149c133
Cazeva <72704263+Cazeva@users.noreply.github.com>
2023-11-07 20:11:11 +0100

    SignUp Views

--- Mizzac/DashBoard/views.py
+++ Mizzac/DashBoard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')  # Assurez-vous que l'URL 'login' est définie dans urls.py
+    template_name = 'dashboard/signup.html'  # Le chemin du template d'inscription
