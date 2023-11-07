b7c533be0070c86dd9a0496082d90d5219920173
Cazeva <72704263+Cazeva@users.noreply.github.com>
2023-11-07 20:39:26 +0100

    login corriger view, wait for chatGPT

--- Mizzac/DashBoard/urls.py
+++ Mizzac/DashBoard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SignUpView
 
 urlpatterns = [
-    path('login/', LogInView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='dashboard/login.html'), name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
 
 ]
--- Mizzac/DashBoard/views.py
+++ Mizzac/DashBoard/views.py
@@ -7,8 +7,3 @@ class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')  # Assurez-vous que l'URL 'login' est définie dans urls.py
     template_name = 'dashboard/signup.html'  # Le chemin du template d'inscription
-
-class LogInView(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('admin')  # Assurez-vous que l'URL 'login' est définie dans urls.py
-    template_name = 'dashboard/login.html'  # Le chemin du template d'inscription
