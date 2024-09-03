faa600f8077249f0fa1ff943fa1322e0dbd07583
Mizeos <thomas3494@outlook.fr>
2024-09-03 17:58:02 +0200

    add save table v1 for skyjo & naval battle

--- Mizzac/GameBoard/models.py
+++ Mizzac/GameBoard/models.py
@@ -24,36 +24,49 @@ class ProfilJoueur(models.Model):
 
     def __str__(self):
         return self.utilisateur.username if self.utilisateur else self.nom_invite
+    
+class SkyJoSave(models.Model):
+    date_time = models.DateTimeField(auto_now_add=True)
+    users = models.JSONField(default=dict, blank=False)
+    scores = models.JSONField(default=dict, blank=False)
+    ranking = models.JSONField(default=list, blank=True)
 
-class Partie(models.Model):
-    jeu = models.ForeignKey(Jeu, on_delete=models.CASCADE, related_name="parties")
-    date_debut = models.DateTimeField(auto_now_add=True)
-    date_fin = models.DateTimeField(blank=True, null=True)
-    joueurs = models.ManyToManyField(ProfilJoueur, through='Participation')
+class NavalBattleSave(models.Model):
+    date_time = models.DateTimeField(auto_now_add=True)
+    users = models.JSONField(default=dict, blank=False)
+    game_grid = models.JSONField(default=list, blank=False)
+    calling_shot = models.JSONField(default=list, blank=False) # format : 1A2 first number identify player and second part identify the location of shot
+    ranking = models.JSONField(default=list, blank=True)
 
-    def __str__(self):
-        return f"{self.jeu.nom} - {self.date_debut}"
+# class Partie(models.Model):
+#     jeu = models.ForeignKey(Jeu, on_delete=models.CASCADE, related_name="parties")
+#     date_debut = models.DateTimeField(auto_now_add=True)
+#     date_fin = models.DateTimeField(blank=True, null=True)
+#     joueurs = models.ManyToManyField(ProfilJoueur, through='Participation')
 
-class Participation(models.Model):
-    joueur = models.ForeignKey(ProfilJoueur, on_delete=models.CASCADE)
-    partie = models.ForeignKey(Partie, on_delete=models.CASCADE)
-    score = models.IntegerField(default=0)
+#     def __str__(self):
+#         return f"{self.jeu.nom} - {self.date_debut}"
 
-    def __str__(self):
-        return f"{self.joueur} - {self.partie} - Score: {self.score}"
+# class Participation(models.Model):
+#     joueur = models.ForeignKey(ProfilJoueur, on_delete=models.CASCADE)
+#     partie = models.ForeignKey(Partie, on_delete=models.CASCADE)
+#     score = models.IntegerField(default=0)
 
-class HistoriquePartie(models.Model):
-    partie = models.OneToOneField(Partie, on_delete=models.CASCADE, related_name="historique")
-    date_archive = models.DateTimeField(auto_now_add=True)
+#     def __str__(self):
+#         return f"{self.joueur} - {self.partie} - Score: {self.score}"
 
-    def __str__(self):
-        return f"Historique de {self.partie}"
+# class HistoriquePartie(models.Model):
+#     partie = models.OneToOneField(Partie, on_delete=models.CASCADE, related_name="historique")
+#     date_archive = models.DateTimeField(auto_now_add=True)
 
-class Statistique(models.Model):
-    joueur = models.ForeignKey(ProfilJoueur, on_delete=models.CASCADE, related_name="statistiques")
-    jeu = models.ForeignKey(Jeu, on_delete=models.CASCADE, related_name="statistiques")
-    nombre_victoires = models.IntegerField(default=0)
-    pourcentage_reussite = models.FloatField(default=0.0)
+#     def __str__(self):
+#         return f"Historique de {self.partie}"
 
-    def __str__(self):
-        return f"Stats de {self.joueur} pour {self.jeu.nom}"
+# class Statistique(models.Model):
+#     joueur = models.ForeignKey(ProfilJoueur, on_delete=models.CASCADE, related_name="statistiques")
+#     jeu = models.ForeignKey(Jeu, on_delete=models.CASCADE, related_name="statistiques")
+#     nombre_victoires = models.IntegerField(default=0)
+#     pourcentage_reussite = models.FloatField(default=0.0)
+
+#     def __str__(self):
+#         return f"Stats de {self.joueur} pour {self.jeu.nom}"
