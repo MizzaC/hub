c905ce9e2caaeeefb6ba2f6f2b93eddb79c87c64
Thomas MICHELET <thomas.michelet@edu.esiee.fr>
2024-09-12 23:38:09 +0200

    add date fields at game stats table to specify date of last stats update

--- Mizzac/GameBoard/models.py
+++ Mizzac/GameBoard/models.py
@@ -29,6 +29,7 @@ class GamesStats(models.Model):
     id = models.AutoField(primary_key=True)
     game = #faire une foreign key qui est en lien avec la primary key nom du jeu pour la table Jeu
     game_version = models.CharField(max_length=100, blank=False) #Put Original if it's not a specific version
+    date_time = models.DateTimeField(auto_now_add=True)
     game_stats = models.JSONField(default=dict, blank=False)
     
 class SkyJoSave(models.Model):
